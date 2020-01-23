76a2a9a070efabad57083e6dc92ce1290f478410
karpoghosyan <karpoghosyan99@gmail.com>
2020-01-24 00:43:11 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Green");
+        colors.add("Brown");
+        colors.add("Blue");
+        colors.add("Red");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+    }
+}
